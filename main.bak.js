34040745bb691bd6fa9cf4a5af8e5338134ea69c
YSTYLE <lxy5266@live.com>
2014-12-20 23:33:17 +0800

    第一版本

--- main.bak.js
+++ main.bak.js
@@ -3,7 +3,7 @@ console.info("解析帖子信息。。。");
 var teilist = $("li.j_thread_list.clearfix");
 var baselink = "http://tieba.baidu.com/";
 console.info("设置特别关注用户......");
-var fav = ["lxy5266"];
+var fav = ["w594669442"];
 window.Notification.requestPermission();
 var Notification = window.Notification || window.mozNotification || window.webkitNotification;
 
@@ -12,6 +12,7 @@ teilist.each(function () {
     var adom = $(this).find("a.j_th_tit")[0];
     console.info("解析帖子链接......");
     var link = baselink + $(adom).attr("href");
+    var title = $(adom).attr("title");
     console.info("获得用户信息......");
     var userlist = $(this).find("a.j_user_card");
     userlist.each(function () {
@@ -19,7 +20,7 @@ teilist.each(function () {
         var name = $(this).text();
         if ($.inArray(name, fav) != -1) {
             console.info("扫描到关注用户，打开新窗口......");
-            show(name + "有新帖子", link, link);
+            show(name + "有新帖子", title, link);
             adom.click();
         }
     });
@@ -41,5 +42,6 @@ function show(title, msg, link) {
     window.open(link, "_blank");
     instance.onclick = function () {
         window.open(link, "_blank");
+        instance.cancel();
     };
 }
--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 console.info("解析帖子信息......");
 var teilist = document.getElementsByClassName("j_thread_list clearfix");
 console.info("设置特别关注用户......\n");
-var fav = loadSetting();
+var setting = loadSetting();
+var fav = setting.userlsit;
+console.info(setting.times);
 console.info(fav);
 window.Notification.requestPermission();
 var Notification = window.Notification || window.mozNotification || window.webkitNotification;
@@ -16,15 +18,17 @@ function getlink(links) {
     console.info("查找当前帖子的用户与链接......");
     var teilink = "";
     var username = [];
+    var title = "";
     for (var i = 0, len = links.length; i < len; i++) {
         var alink = links[i];
-        if (alink.className == "j_user_card  ") {
+        if (alink.className.indexOf("j_user_card") != -1) {
             username.push(alink.innerText);
-        } else if (alink.className == "j_th_tit") {
+        } else if (alink.className.indexOf("j_th_tit") != -1) {
             teilink = alink.href;
+            title = alink.title;
         }
     }
-    return {username: username, alink: teilink};
+    return {"username": username, "alink": teilink, "title": title};
 }
 
 /**
@@ -32,14 +36,16 @@ function getlink(links) {
  * @param username
  * @param teilink
  */
-function checkuser(username, teilink) {
+function checkuser(result) {
     console.info("检测用户是否特别关注......");
+    var username = result.username;
     for (var i = 0, len = username.length; i < len; i++) {
         var user = username[i];
+        console.info("标题：" + result.title + "，用户：" + result.username + "，链接：" + result.alink);
         var index = fav.indexOf(user);
         if (index != -1) {
             console.info("扫描到关注用户，打开新窗口......");
-            show(user + "有新帖子", teilink, teilink);
+            show(user + "有新帖子", result.title, result.alink);
         }
     }
 }
@@ -51,9 +57,7 @@ for (var i = 0, len = teilist.length; i < len; i++) {
     console.info("查找当前帖子[" + (i + 1) + "]的链接......");
     var links = teilist[i].getElementsByTagName("a");
     var result = getlink(links);
-    var teilink = result.alink;
-    var username = result.username;
-    checkuser(username, teilink);
+    checkuser(result);
     console.info("\n")
 }
 
@@ -73,12 +77,29 @@ function show(title, msg, link) {
     window.open(link, "_blank");
     instance.onclick = function () {
         window.open(link, "_blank");
+        instance.cancel();
     };
 }
 
+/**
+ * 加载设置
+ * @returns {{userlsit: (Array|*), times: *}}
+ */
 function loadSetting() {
     var rt = window.external.mxGetRuntime();
     var str = rt.storage.getConfig("username");
+    var times = rt.storage.getConfig("times");
     userlsit = str.split(',');
-    return userlsit;
+    return {"userlsit": userlsit, "times": times};
+}
+
+/**
+ * 刷新页面
+ */
+function timer(){
+    location.reload();
+}
+
+if(location.href.indexOf("tieba.baidu.com") != -1){
+    var times = setInterval(timer, setting.times * 1000);
 }
